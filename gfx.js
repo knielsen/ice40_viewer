c5d516d44af8689b4a756d25cdfb0f211f5564dc
Kristian Nielsen <knielsen@knielsen-hq.org>
2016-11-16 08:33:40 +0100

    Initial drawing of span4 and span12 routing wires.
    This just draws all the wires in logic and bram-cells. It's rather a mess
    due to the large number of wires, but it's a start.

--- gfx.js
+++ gfx.js
@@ -25,6 +25,18 @@ function worldFillRect(canvas, wx0, wy0, wx1, wy1) {
 }
 
 
+function worldMoveTo(canvas, c, wx, wy) {
+    var a = world2canvas(canvas, wx, wy);
+    c.moveTo(a[0], a[1]);
+}
+
+
+function worldLineTo(canvas, c, wx, wy) {
+    var a = world2canvas(canvas, wx, wy);
+    c.lineTo(a[0], a[1]);
+}
+
+
 function mk_tiles(chipdb) {
     var ts;
     var x, y;
@@ -62,7 +74,179 @@ function tileCol(typ, active) {
 }
 
 
-var g_dbg;
+var tileEdge = 0.42;
+var wireSpc = 0.0102;
+var span4Base = - 0.4;
+var span12Base = 0.4 - (12*2-1)*wireSpc;
+var spanShort = -0.37;
+var spanShort2 = -0.282;
+
+
+function drawTilesSpan4H(canvas, c, x, y, tile, chipdb) {
+    var i, j;
+
+    // Draw horizontal span4's.
+    for (i = 0; i < 4; ++i) {
+	c.strokeStyle = "#00003F";
+	c.lineWidth = 1;
+	c.beginPath();
+
+	for (j = 0; j < 12; ++j) {
+	    worldMoveTo(canvas, c, x - 0.5, y+span4Base + (13*i+j)*wireSpc);
+	    if (i != 3) {
+		if ((j % 2) == 0) {
+		    worldLineTo(canvas, c, x - (7-20)*wireSpc, y+span4Base + (13*i+j)*wireSpc);
+		    worldLineTo(canvas, c, x + (6+20)*wireSpc, y+span4Base + (13*(i+1)+j+1)*wireSpc);
+		    worldLineTo(canvas, c, x + 0.5, y+span4Base + (13*(i+1)+j+1)*wireSpc);
+		} else {
+		    worldLineTo(canvas, c, x - (5-20)*wireSpc, y+span4Base + (13*i+j)*wireSpc);
+		    worldLineTo(canvas, c, x + (6+20)*wireSpc, y+span4Base + (13*(i+1)+j-1)*wireSpc);
+		    worldLineTo(canvas, c, x + 0.5, y+span4Base + (13*(i+1)+j-1)*wireSpc);
+		}
+	    } else {
+		worldLineTo(canvas, c, x + spanShort, y + span4Base + (13*3+j)*wireSpc);
+		worldMoveTo(canvas, c, x - spanShort, y + span4Base + j*wireSpc);
+		worldLineTo(canvas, c, x + 0.5, y + span4Base + j*wireSpc);
+	    }
+	}
+	c.stroke();
+    }
+}
+
+
+function drawTilesSpan12H(canvas, c, x, y, tile, chipdb) {
+    // Draw horizontal span12's.
+    for (i = 0; i < 12; ++i) {
+	c.strokeStyle = "#00003F";
+	c.lineWidth = 1;
+	c.beginPath();
+
+	for (j = 0; j < 2; ++j) {
+	    worldMoveTo(canvas, c, x - 0.5, y+span12Base + (2*i+j)*wireSpc);
+	    if (i != 11) {
+		if ((j % 2) == 0) {
+		    worldLineTo(canvas, c, x - 2*wireSpc, y+span12Base + (2*i+j)*wireSpc);
+		    worldLineTo(canvas, c, x + 1*wireSpc, y+span12Base + (2*(i+1)+j+1)*wireSpc);
+		    worldLineTo(canvas, c, x + 0.5, y+span12Base + (2*(i+1)+j+1)*wireSpc);
+		} else {
+		    worldLineTo(canvas, c, x - 0*wireSpc, y+span12Base + (2*i+j)*wireSpc);
+		    worldLineTo(canvas, c, x + 1*wireSpc, y+span12Base + (2*(i+1)+j-1)*wireSpc);
+		    worldLineTo(canvas, c, x + 0.5, y+span12Base + (2*(i+1)+j-1)*wireSpc);
+		}
+	    } else {
+		worldLineTo(canvas, c, x + spanShort, y + span12Base + (2*11+j)*wireSpc);
+		worldMoveTo(canvas, c, x - spanShort, y + span12Base + j*wireSpc);
+		worldLineTo(canvas, c, x + 0.5, y + span12Base + j*wireSpc);
+	    }
+	}
+	c.stroke();
+    }
+}
+
+
+function drawTilesSpan4V(canvas, c, x, y, tile, chipdb) {
+    var i, j;
+
+    // Draw vertical span4's.
+    for (i = 0; i < 4; ++i) {
+	c.strokeStyle = "#3F0000";
+	c.lineWidth = 1;
+	c.beginPath();
+
+	for (j = 0; j < 12; ++j) {
+	    worldMoveTo(canvas, c, x+span4Base + (13*i+j)*wireSpc, y + 0.5);
+	    if (i != 3) {
+		if ((j % 2) == 0) {
+		    worldLineTo(canvas, c, x+span4Base + (13*i+j)*wireSpc, y + (7+20)*wireSpc);
+c.lineWidth = 3;
+		    worldLineTo(canvas, c, x+span4Base + (13*(i+1)+j+1)*wireSpc, y - (6-20)*wireSpc);
+c.lineWidth = 1;
+		    worldLineTo(canvas, c, x+span4Base + (13*(i+1)+j+1)*wireSpc, y - 0.5);
+		} else {
+		    worldLineTo(canvas, c, x+span4Base + (13*i+j)*wireSpc, y + (5+20)*wireSpc);
+		    worldLineTo(canvas, c, x+span4Base + (13*(i+1)+j-1)*wireSpc, y - (6-20)*wireSpc);
+		    worldLineTo(canvas, c, x+span4Base + (13*(i+1)+j-1)*wireSpc, y - 0.5);
+		}
+	    } else {
+		worldLineTo(canvas, c, x + span4Base + (13*3+j)*wireSpc, y - spanShort);
+		worldMoveTo(canvas, c, x + span4Base + j*wireSpc, y + spanShort2);
+		worldLineTo(canvas, c, x + span4Base + j*wireSpc, y - 0.5);
+	    }
+	    worldMoveTo(canvas, c, x+span4Base + (13*i+j)*wireSpc,
+			y+span4Base + (13*i+j-0.5)*wireSpc);
+	    worldLineTo(canvas, c, x-0.5, y+span4Base + (13*i+j-0.5)*wireSpc);
+	    worldMoveTo(canvas, c, x-spanShort-wireSpc, y+span4Base + (13*i+j-0.5)*wireSpc);
+	    worldLineTo(canvas, c, x+0.5, y+span4Base + (13*i+j-0.5)*wireSpc);
+	}
+	c.stroke();
+    }
+    // Draw interconnect dots.
+    for (i = 0; i < 4; ++i) {
+	c.strokeStyle = "#3F0000";
+	c.lineWidth = 5;
+	var oldCap = c.lineCap;
+	c.lineCap = "round";
+	c.beginPath();
+
+	for (j = 0; j < 12; ++j) {
+	    worldMoveTo(canvas, c, x+span4Base + (13*i+j)*wireSpc,
+			y+span4Base + (13*i+j-0.5)*wireSpc);
+	    worldLineTo(canvas, c, x+span4Base + (13*i+j)*wireSpc,
+			y+span4Base + (13*i+j-0.5)*wireSpc);
+	}
+	c.stroke();
+	c.lineCap = oldCap;
+    }
+}
+
+
+function drawTilesSpan12V(canvas, c, x, y, tile, chipdb) {
+    // Draw vertical span12's.
+    for (i = 0; i < 12; ++i) {
+	c.strokeStyle = "#3F0000";
+	c.lineWidth = 1;
+	c.beginPath();
+
+	for (j = 0; j < 2; ++j) {
+	    worldMoveTo(canvas, c, x+span12Base + (2*i+j)*wireSpc, y + 0.5);
+	    if (i != 11) {
+		if ((j % 2) == 0) {
+		    worldLineTo(canvas, c, x+span12Base + (2*i+j)*wireSpc, y + 2*wireSpc);
+		    worldLineTo(canvas, c, x+span12Base + (2*(i+1)+j+1)*wireSpc, y - 1*wireSpc);
+		    worldLineTo(canvas, c, x+span12Base + (2*(i+1)+j+1)*wireSpc, y - 0.5);
+		} else {
+		    worldLineTo(canvas, c, x+span12Base + (2*i+j)*wireSpc, y + 0*wireSpc);
+		    worldLineTo(canvas, c, x+span12Base + (2*(i+1)+j-1)*wireSpc, y - 1*wireSpc);
+		    worldLineTo(canvas, c, x+span12Base + (2*(i+1)+j-1)*wireSpc, y - 0.5);
+		}
+	    } else {
+		worldLineTo(canvas, c, x + span12Base + (2*11+j)*wireSpc, y - spanShort);
+		worldMoveTo(canvas, c, x + span12Base + j*wireSpc, y + spanShort);
+		worldLineTo(canvas, c, x + span12Base + j*wireSpc, y - 0.5);
+	    }
+	}
+	c.stroke();
+    }
+}
+
+
+function drawTileWires(canvas, x, y, tile, chipdb) {
+    var c = canvas.getContext("2d");
+    var i, j;
+
+    if (tile.typ != 'io') {
+	// ToDo: io tile needs some differences here.
+	drawTilesSpan4H(canvas, c, x, y, tile, chipdb);
+	drawTilesSpan4V(canvas, c, x, y, tile, chipdb);
+    }
+
+    if (tile.typ != 'io') {
+	drawTilesSpan12H(canvas, c, x, y, tile, chipdb);
+	drawTilesSpan12V(canvas, c, x, y, tile, chipdb);
+    }
+}
+
+
 function drawTiles(canvas, ts, chipdb) {
     var c = canvas.getContext("2d");
     var x0 = Math.floor(view_x0 - 0.5);
@@ -71,6 +255,13 @@ function drawTiles(canvas, ts, chipdb) {
     var y1 = Math.ceil(view_y1 + 0.5);
 
     var x, y;
+    var width = canvas.width;
+    var height = canvas.height;
+
+    // Depending on how many pixels per tile (zoom level), we draw
+    // with different level of detail.
+    var tile_pixels =
+	0.5*(canvas.width/(view_x1 - view_x0) + canvas.height/(view_y1 - view_y0));
 
     for (y = y0; y < y1; ++y) {
 	for (x = x0; x < x1; ++x) {
@@ -79,7 +270,9 @@ function drawTiles(canvas, ts, chipdb) {
 	    var tile = ts[y][x];
 	    var col = tileCol(tile.typ, tile.active);
 	    c.fillStyle = col;
-	    worldFillRect(canvas, x-0.4, y-0.4, x+0.4, y+0.4);
+	    worldFillRect(canvas, x-tileEdge, y-tileEdge, x+tileEdge, y+tileEdge);
+	    if (tile_pixels > 200)
+		drawTileWires(canvas, x, y, tile, chipdb);
 	}
     }
 }
