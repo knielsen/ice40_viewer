c2c408938a905909a54c2618517532c98e0feadc
Kristian Nielsen <knielsen@knielsen-hq.org>
2017-01-18 20:39:01 +0100

    Use inline javascript in standalone mode
    In standalone mode, copy the various referenced *.js files directly into the
    generated .html file, rather than referencing them with file:/// urls.
    
    This helps avoid version problems when ice40_viewer is upgraded. With the
    file:/// urls, an old output .html might reference new, replaced .js files,
    which is likely to cause problems.
    
    The chipdb-8k.txt.js is still left as file:/// reference. This file is 40
    MB, so not nice to include in every output file, and it is not likely to
    change in an incompatible way (or at all).

--- iceview_html.py
+++ iceview_html.py
@@ -37,32 +37,34 @@ html_out_filename = args[1]
 html_in_filename = basedir + "/" + "ice40_viewer.html"
 
 # Copy the ice40_viewer.html file with appropriate replacements.
-def js_absurl(m):
-    return '<script type="text/javascript" src="file://' + basedir + '/' + m.group(1) + '">'
 discard = False
 with open(html_in_filename, "r") as f:
-    with open(html_out_filename, "w") as h:
+    with open(html_out_filename, "w") as g:
         for line in f:
-            line2 = line.replace("var standalone_mode = false",
-                                 "var standalone_mode = true")
-            line3 = re.sub(r'<script type="text/javascript" src="([^"]+)">', js_absurl, line2)
-            if line3 == "    <!-- Start skip in standalone mode -->\n":
+            line = line.replace("var standalone_mode = false",
+                                "var standalone_mode = true")
+            # Inline referenced javascript files.
+            m = re.match(r'^ *<script type="text/javascript" src="([^"]+)"></script> *$', line)
+            if m:
+                js = open(basedir + "/" + m.group(1)).read()
+                line = "<script>\n" + js + "\n</script>\n";
+            elif line == "    <!-- Start skip in standalone mode -->\n":
                 discard = True
             if not discard:
-                h.write(line3)
-            if line3 == "    <!-- End skip in standalone mode -->\n":
+                g.write(line)
+            if line == "    <!-- End skip in standalone mode -->\n":
                 discard = False
-            if line3 == "  <!-- Standalone chipdb included here -->\n":
+            if line == "  <!-- Standalone chipdb included here -->\n":
                 # print out the statement to load the chipdb.
-                h.write('  <script type="text/javascript" src="file://' + basedir +
+                g.write('  <script type="text/javascript" src="file://' + basedir +
                         '/chipdb-8k.txt.js"></script>\n')
-            if line3 == "// Standalone file data goes here.\n":
+            elif line == "// Standalone file data goes here.\n":
                 # Print out the embedded .asc contents.
-                h.write("g_fileData = \"\\\n")
-                with open(asc_filename, "r") as g:
-                    for a in g:
-                        h.write(a.replace("\n", "\\n\\\n"))
-                h.write("\";\n")
+                g.write("g_fileData = \"\\\n")
+                with open(asc_filename, "r") as h:
+                    for a in h:
+                        g.write(a.replace("\n", "\\n\\\n"))
+                g.write("\";\n")
 
 if spawn_browser:
     url = "file://" + os.path.abspath(html_out_filename)
