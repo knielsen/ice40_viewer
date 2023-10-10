f7857940addf6817ffd3a4b32026ede1ccdd967c
Kristian Nielsen <knielsen@knielsen-hq.org>
2023-10-10 08:08:49 +0200

    Add fomu_rfb to example menu and fix indentations
    Signed-off-by: Kristian Nielsen <knielsen@knielsen-hq.org>

--- myutil.js
+++ myutil.js
@@ -225,38 +225,38 @@ for (i = 0; i < 5000; ++i) {
 	break;
     case "colbuf":
 	break;
-	case "dsp0_tile":
+    case "dsp0_tile":
 	init_func = tile_init;
 	break
-	case "dsp0_tile_bits":
+    case "dsp0_tile_bits":
 	init_func = init_single_args(["columns", "rows"]);
 	content_func = tile_bits_add;
 	break
-	case "dsp1_tile":
+    case "dsp1_tile":
 	init_func = tile_init;
 	break
-	case "dsp1_tile_bits":
+    case "dsp1_tile_bits":
 	init_func = init_single_args(["columns", "rows"]);
 	content_func = tile_bits_add;
 	break
-	case "dsp2_tile":
+    case "dsp2_tile":
 	init_func = tile_init;
 	break
-	case "dsp2_tile_bits":
+    case "dsp2_tile_bits":
 	init_func = init_single_args(["columns", "rows"]);
 	content_func = tile_bits_add;
 	break
-	case "dsp3_tile":
+    case "dsp3_tile":
 	init_func = tile_init;
 	break
-	case "dsp3_tile_bits":
+    case "dsp3_tile_bits":
 	init_func = init_single_args(["columns", "rows"]);
 	content_func = tile_bits_add;
 	break
-	case "ipcon_tile":
+    case "ipcon_tile":
 	init_func = tile_init;
 	break
-	case "ipcon_tile_bits":
+    case "ipcon_tile_bits":
 	init_func = init_single_args(["columns", "rows"]);
 	content_func = tile_bits_add;
 	break
